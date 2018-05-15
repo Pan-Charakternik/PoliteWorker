ee961a2ab9c517f330e88d794f37aaf62f77bea7
Pan-Charakternik <35796629+Pan-Charakternik@users.noreply.github.com>
2018-05-15 16:33:49 +0200

    Initial commit

---
+++ src/com/jjozwik/Main.java
@@ -0,0 +1,14 @@
+package com.jjozwik;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        final Worker worker1 = new Worker("Worker1", true);
+        final Worker worker2 = new Worker("Worker2", true);
+
+        
+
+    }
+}
